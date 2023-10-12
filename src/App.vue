d6a291811788e5012dac1c60f3b7aa32e640fcc2
lucklcy <lucklcy@hotmail.com>
2023-10-12 19:49:53 +0800

    on release-git-test commit 001

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@
       <button
         class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
       >
-        Message
+        Message14141
       </button>
     </div>
   </div>
