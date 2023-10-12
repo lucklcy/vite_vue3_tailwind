d8f41dccfa16dc3384673d092898c37159fa5c1e
lucklcy <lucklcy@hotmail.com>
2023-10-12 20:03:49 +0800

    on main add component/base/search.vue

---
+++ src/components/base/search.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="component-base-search"></div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "c-example",
+};
+</script>
+
+<script setup lang="ts">
+export interface Props {
+  type: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  type: "new",
+});
+</script>
+
+<style lang="less" scoped>
+.component-base-search {
+  background-color: #fff;
+}
+</style>
