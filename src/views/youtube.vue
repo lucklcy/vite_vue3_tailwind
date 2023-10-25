741afc221767d44c2f24e7e2f4c71e27eef9479d
liuchongyang <lucklcy@hotmail.com>
2023-10-26 01:17:36 +0800

    01:14

--- src/views/youtube.vue
+++ src/views/youtube.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="h-14 w-full flex">
+  <div class="h-14 w-full flex flex-row justify-between items-center">
     <div class="flex flex-row justify-around w-44 h-full items-center px-2 cursor-pointer">
-      <div class="border-transparent border-0 rounded-full p-2 dark:hover:bg-slate-400/30">
+      <div
+        class="border-transparent border-0 rounded-full p-2 dark:hover:bg-gray-400/30 hover:bg-gray-100"
+      >
         <ExpandIcon width="24" height="24" fill-color="#fff"></ExpandIcon>
       </div>
       <div class="w-32 h-5 dark:fill-white">
         <YoutubeIcon></YoutubeIcon>
       </div>
+    </div>
+    <div
+      class="bg-transparent border rounded-3xl border-gray-800 h-10 px-4 flex flex-row justify-between items-center"
+    >
+      <input
+        type="text"
+        placeholder="搜索"
+        class="bg-none bg-transparent leading-10 outline-none"
+      />
     </div>
   </div>
 </template>
